1cd81cb78623347f5bfaed18180fc2905bc1904c
MiguelVargass82 <147115772+MiguelVargass82@users.noreply.github.com>
2024-03-30 22:15:27 -0500

    Implementacion del los metodos (1) (0)
    Al abrir los archivos en otro PC se daño el Qt y me denego unos permisos y se crearon muchos archivos que son los que estan apareciendo en este commit, entonces decidi crear un nuevo proyecto y copie y pegue los modulos y ya me funciono

--- QT proyect/Parcial_1MiSt/ModuloClave.cpp
+++ QT proyect/Parcial_1MiSt/ModuloClave.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 using namespace std;
+int** crearM(int num);
+void liberarM(int** matriz, int longitud);
+int** rotarM(int** matriz,int tamaño, int modo);
+
 
 int* agregarDato(int dato, int* arreglo, int longitud){   //Esta funcion va a alargar el arreglo una posicion y luego le va a agregar el dato que necesitamos
     int* nuevoArreglo = new int[longitud+1];
@@ -15,7 +19,7 @@ int* agregarDato(int dato, int* arreglo, int longitud){   //Esta funcion va a al
     return nuevoArreglo;
 }
 
-int* pedirClave(){
+int* pedirClave(int& tamaño){
     cout<<"Ingresa numero por numero la clave a la cual le gustaria encontrar su cerradura, cuanto termine ingrese porfavor (-2) "<<endl;
     int* arreglo = new int[1];
     int cont=0;
@@ -25,7 +29,7 @@ int* pedirClave(){
         cout<<"Ingresa el parametro numero "<<cont+1<<":";
         cin>>numero;
         if(cont==0 || cont==1){       //Esta ingresando las coordenandas de la matriz
-            if(numero<=1){     //Ingreso un numero no valido
+            if(numero<1){     //Ingreso un numero no valido
                 cout<<"No puede ingresar un numero negativo porfavor ingresa uno diferente: "<<endl;
             }
             else{
@@ -62,9 +66,47 @@ int* pedirClave(){
         }
     }   //Fin while
 
-    for (int i=0; i<cont;i++){
-        cout<<arreglo[i];
-        cout<<" ";
-    }
+    tamaño = cont;
     return arreglo;
+}
+
+
+int caso1(int valor,int& casillaModo, int tamañoPre, int coorX, int coorY){
+    // (1) La siguiente matriz debe ser menor en las coordenadas
+   //Retornara la siguiente matriz
+
+    int tamaño= tamañoPre;
+    int** matriz = crearM(tamañoPre);
+    int modo=0;
+       //Nos servira para saber si ya roto todas las veces y no sirve
+    bool ban=true;
+    while(ban){
+        if(matriz[coorX][coorY]<valor){ //Encotramos la matriz que era
+            liberarM(matriz,tamaño);
+            casillaModo=modo;
+            return tamaño;
+        }
+        else if (tamaño==3){   //No es posible realizar una cerradura;
+            return -1;
+
+        }else{      //No nos sirve la matriz creada anteriormente
+            bool ban2=true;
+            while(ban2){        //Ciclo para las rotaciones
+                 int** nuevaMat = rotarM(matriz,tamaño, modo);
+                if(nuevaMat[coorX][coorY]>=valor && modo<4){  //No cumple
+                    modo++;
+                    liberarM(nuevaMat,tamaño);
+
+                }else if (modo==4){ //Ya las roto de todas las formas
+                    ban2=false;
+                    modo=0;
+                    tamaño=tamaño-2;
+                }else{
+                    ban2=false;
+
+                }
+            }
+        }
+    }
+    return tamaño;
 }
--- QT proyect/Parcial_1MiSt/main.cpp
+++ QT proyect/Parcial_1MiSt/main.cpp
@@ -1,24 +1,79 @@
 #include <iostream>
 using namespace std;
 
+//Funciones Estructura M
 void mostrarM(int** matriz,int tamaño);
 int** crearM(int num);
 int** rotarM(int** matriz,int tamaño, int modo);
 void liberarM(int** matriz, int longitud);
+int mayorModo(int coorX, int coorY, int tamaño);
+
+//Funciones de Modulo clave
 int* agregarDato(int dato, int* arreglo, int longitud); //Esta funcion es necesaria para pedirClave()
-int* pedirClave();
+int* pedirClave(int& tamaño);
+int caso1(int valor,int& casillaModo, int tamañoPre, int coorX, int coorY);
+
+
 
 int main()
 {
+    //Lo primero que vamos a hacer es solicitar la clave
+    //La funcion de pedirClave nos entregara un arreglo dinamico con los parametros de la clave
+    //Le pasaremos un parametro por referencia que nos va ayudar a determinar el tamaño del arreglo dinamico
 
-    int modo=3;
-    int tamaño=3;
-    int** matrizPrueba = crearM(tamaño);
-    mostrarM(matrizPrueba, tamaño);
+    int tamaño=0;   //tamaño del arreglo de la clave
 
-    cout<<"MODO "<<modo<<" --------------------"<<endl;
+    int* clave = pedirClave(tamaño);    //Almacenamos aca los parametros de la calve
+    int* cerradura = new int[tamaño-1]; //Arreglo que almacenara los tamaños de las matrices
+    int* modos = new int[tamaño-1]; //Nos aguardara los modos en los que necesitamos cada matriz
 
-    mostrarM(rotarM(matrizPrueba,tamaño,modo),tamaño);
+    bool ban = true;
+
+     //Seran la posicion de los arreglos de la clave y de la cerradura
+    int posClave=2; //Inicia desde 2 para que comience recorriendo las condiciones
+    int posCerra=0; //Podemos usar esta variable tanto para la cerradura como para el arreglo de modos
+
+
+    //Cordenadas
+    int posX= clave[0];
+    int posY= clave[1];
+    int coorMayor =0;
+    if (posX>posY){
+        coorMayor=posX;
+    }else{
+        coorMayor=posY;
+    }
+
+
+    int tamañoPre=tamaño+coorMayor;
+    if(tamañoPre%2==0){    //Tamaño prederterminado que le queremos dar a las matrices
+        //Si es par le restamos 1, recordemos que las dimensiones de las matrices siempre es impar
+        tamañoPre=tamañoPre-1;
+    }
+
+    //La primera matriz de la cerradura tendra un tamaño=tamaño+la coordenada mayor para abarcar mas facil el caso K(n1,n2,1,1,1,...)
+    int** matrizU = crearM(tamañoPre);
+    cerradura[posCerra]=tamañoPre;
+    modos[posCerra]=0;  //La matriz base siempre va a estar en modo 0
+
+    while(ban){
+        posCerra++;
+        int valor=matrizU[posX][posY];
+        if(clave[posClave]==1){ //Caso (1) A>B
+            int sigMat=caso1(valor,modos[posCerra],tamañoPre,posX,posY);
+            cerradura[posCerra]=sigMat; //Agregamos al arreglo el tamaño de la matriz, el modo se agregara automaticamente por referencia
+        }
+
+
+        if(posClave==tamaño-1){ //Condicion de terminacion, termino de recorrer la clave
+            ban=false;
+        }
+        posClave++;
+    }
+
+    for(int i=0;i<tamaño-1;i++){
+        cout<<cerradura[i]<<" ";
+    }
 
 
     return 0;
