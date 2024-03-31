adef6e1fc35f6089e170c70261b597aea3a69699
MiguelVargass82 <147115772+MiguelVargass82@users.noreply.github.com>
2024-03-31 00:18:01 -0500

    Agreamos la funcionalidad (-1)
    Me toco crear otro archivo porque otra vez se me daño el anterior, pero creamos la funcion para cualdo el usuario ponga -1

---
+++ Parcial 1 Codigo/Parcial1_MigSt/ModuloEstructurasM.cpp
@@ -0,0 +1,177 @@
+#include<iostream>
+using namespace std;
+int** crearM(int num){  //Funcion que nos crea la estructura planteada en el problema
+
+    //Solo recibe numeros enteros impares
+    int vacio= int(num/2);
+
+
+    int** matriz = new int*[num];   //Creamos el tamaño de la estrucutura M
+    for (int i=0; i<num;i++){
+        matriz[i]= new int[num];
+    }
+
+    int cont=1;
+    for(int i=0;i<num;i++){
+        for(int j=0; j<num;j++){
+
+            if(!(i==vacio  && j==vacio)){      //Si no es el centro de la matriz
+                matriz[i][j]=cont;
+                cont++;
+            }
+            else{
+                matriz[i][j]=0;
+            }
+        }
+    }
+    return matriz;
+}
+
+
+
+void mostrarM(int** matriz,int tamaño){    //Nos muestra la matriz en consola, el segundo parametro...
+
+    for(int i=0;i<tamaño;i++){            //...es una dimension de la amtriz la cual debe tener las mismas dimensiones
+        for(int j=0; j<tamaño;j++){
+            std:: cout<<matriz[i][j];
+            std:: cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+
+int** rotarM(int** matriz,int tamaño, int modo){   //Funcion de rotacion de matrices dependiendo del modo
+
+    //Creamos una matriz auxiliar
+    int** matrizAux = new int*[tamaño];
+    for(int i=0; i<tamaño;i++){
+        matrizAux[i]= new int[tamaño];
+    }
+
+    for(int i =0;i<tamaño;i++){
+        for(int j =0;j<tamaño;j++){
+            matrizAux[i][j]=matriz[i][j];
+        }
+    }
+
+
+    //La inicializacion de la matriz auxiliar sera la matriz rotada como tal
+
+    int posX=0;
+    int posY=0;
+
+    switch (modo){  //Dependiendo el modo va a rotar la matriz de cierta manera
+
+    case 1:
+        posX=0;
+        posY=0;
+        for (int i =0;i<tamaño;i++){
+            posX=0;
+            for(int j=tamaño-1;j>=0;j--){           //I y J recorren la matriz 1 con normalidad de abajo hacia arriba
+                matrizAux[posX][posY]=matriz[i][j];
+                posX++;
+            }
+            posY++;
+        }
+        return matrizAux;
+        break;
+
+    case 2:
+        posX=tamaño-1;
+        posY=tamaño-1;
+        for (int i =0;i<tamaño;i++){
+            posY=tamaño-1;
+            for(int j=0;j<tamaño;j++){           //I y J recorren la matriz 1 con normalidad de abajo hacia arriba
+                matrizAux[posX][posY]=matriz[i][j];
+                posY--;
+            }
+            posX--;
+        }
+        return matrizAux;
+        break;
+
+
+    case 3:
+        posX=0;
+        posY=0;
+        for (int i =tamaño-1;i>=0;i--){
+            posX=0;
+            for(int j=0;j<tamaño;j++){           //I y J recorren la matriz 1 con normalidad de abajo hacia arriba
+                matrizAux[posX][posY]=matriz[i][j];
+                posX++;
+            }
+            posY++;
+        }
+        return matrizAux;
+        break;
+
+
+    case 0:
+        std::cout<<"Dejamos igual la matriz "<<std::endl;
+        return matrizAux;
+        break;
+
+    default:
+        std::cout<<"No se esta ingresando uno valido "<<std::endl;
+        return matrizAux;
+        break;
+    }
+}
+
+
+
+void liberarM(int**& matriz,int longitud)
+{   //Funcion que liberas posiciones de memoria dinamica de la matriz
+
+    for(int i=0;i<longitud;i++){
+        delete[] matriz[i];
+    }
+    delete[] matriz;
+    matriz=nullptr;
+}
+
+
+
+
+int mayorModo(int coorX, int coorY, int tamaño){
+    //Funcion que nos haya el modo donde las coordenadas ingresadas son las mayores
+
+    int** matriz0 = crearM(tamaño);
+    int** matriz1= rotarM(matriz0, tamaño, 1);
+    int** matriz2= rotarM(matriz0, tamaño, 2);
+    int** matriz3= rotarM(matriz0, tamaño, 3);
+    //  Creamos las matrices en los tres modos y en los tre s
+    int num0 = matriz0[coorX][coorY];
+    int num1 = matriz1[coorX][coorY];
+    int num2 = matriz2[coorX][coorY];
+    int num3 = matriz3[coorX][coorY];
+
+    int mayor=0;
+
+    if(num0>=num1 && num0>=num2 && num0>=num3){
+        mayor=0;
+    }else if(num1>=num0 && num1>=num2 && num1>=num3){
+        mayor=1;
+    } else if (num2>=num1 && num2>=num0 && num2>=num3){
+        mayor=2;
+    }else if (num3>=num1 && num3>=num2 && num3>=num0){
+        mayor= 3;
+    }
+
+    liberarM(matriz0,tamaño);
+    liberarM(matriz1,tamaño);
+    liberarM(matriz2,tamaño);
+    liberarM(matriz3,tamaño);
+
+    return mayor;
+}
+
+
+
+
+
+
+
+
+
---
+++ Parcial 1 Codigo/Parcial1_MigSt/main.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+using namespace std;
+
+//Funciones Estructura M
+void mostrarM(int** matriz,int tamaño);
+int** crearM(int num);
+int** rotarM(int** matriz,int tamaño, int modo);
+void liberarM(int**& matriz, int longitud);
+int mayorModo(int coorX, int coorY, int tamaño);
+
+//Funciones de Modulo clave
+int* agregarDato(int dato, int* arreglo, int longitud); //Esta funcion es necesaria para pedirClave()
+int* pedirClave(int& tamaño);
+int caso1(int valor,int& casillaModo, int tamañoPre, int coorX, int coorY);
+int caso2(int valor,int& casillaModo, int tamañoPre, int coorX, int coorY);
+
+
+int main()
+{
+    //Lo primero que vamos a hacer es solicitar la clave
+    //La funcion de pedirClave nos entregara un arreglo dinamico con los parametros de la clave
+    //Le pasaremos un parametro por referencia que nos va ayudar a determinar el tamaño del arreglo dinamico
+
+    int tamaño=0;   //tamaño del arreglo de la clave
+
+    int* clave = pedirClave(tamaño);    //Almacenamos aca los parametros de la calve
+    int* cerradura = new int[tamaño-1]; //Arreglo que almacenara los tamaños de las matrices
+    int* modos = new int[tamaño-1]; //Guardara los modos en los que necesitamos cada matriz
+    // int* valores = new int[tamaño-1];   //Almacenara los valores de cada matriz
+
+    bool ban = true;
+
+    //Seran la posicion de los arreglos de la clave y de la cerradura
+    int posClave=2; //Inicia desde 2 para que comience recorriendo las condiciones
+    int posCerra=0; //Podemos usar esta variable tanto para la cerradura como para el arreglo de modos
+
+
+    //Cordenadas
+    int posX= clave[0];
+    int posY= clave[1];
+    int coorMayor =0;
+    if (posX>posY){
+        coorMayor=posX;
+    }else{
+        coorMayor=posY;
+    }
+
+
+    int tamañoPre=(tamaño-1)+coorMayor;
+    if(tamañoPre%2==0){    //Tamaño prederterminado que le queremos dar a las matrices
+        //Si es par le restamos 1, recordemos que las dimensiones de las matrices siempre es impar
+        tamañoPre=tamañoPre-1;
+    }
+
+    bool error = false;
+
+    //La primera matriz de la cerradura tendra un tamaño=tamaño+la coordenada mayor para abarcar mas facil el caso K(n1,n2,1,1,1,...)
+    int** matrizU = crearM(tamañoPre);
+    cerradura[posCerra]=tamañoPre;
+    modos[posCerra]=0;  //La matriz base siempre va a estar en modo 0
+
+    int tamNuevMatr= tamañoPre;
+    while(ban){
+
+        posCerra++;
+        int valor=matrizU[posX][posY];
+
+        if(clave[posClave]==1){ //Caso (1) A>B
+
+            liberarM(matrizU, tamNuevMatr);
+            tamNuevMatr=caso1(valor,modos[posCerra],tamañoPre,posX,posY);
+
+            if(tamNuevMatr!=-1){    //No hubo ningun error
+
+                cerradura[posCerra]=tamNuevMatr; //Agregamos al arreglo el tamaño de la matriz, el modo se agregara automaticamente por referencia
+                int** matrizAux=crearM(tamNuevMatr);
+                matrizU=rotarM(matrizAux,tamNuevMatr, modos[posCerra]);
+                liberarM(matrizAux, tamNuevMatr);
+
+            }else{  //Hubo un error
+
+                error= true;
+                ban=false;
+
+            }
+
+        }else if(clave[posClave]==0){
+            cerradura[posCerra]=tamNuevMatr; //Agregamos al arreglo el tamaño de la matriz
+            modos[posCerra]=modos[posCerra-1];
+        }
+        else if (clave[posClave]==-1){  //Caso (-1)
+
+               liberarM(matrizU, tamNuevMatr);
+               tamNuevMatr=caso2(valor,modos[posCerra],tamañoPre,posX,posY);
+                cerradura[posCerra]=tamNuevMatr; //Agregamos al arreglo el tamaño de la matriz, el modo se agregara automaticamente por referencia
+                int** matrizAux=crearM(tamNuevMatr);
+                matrizU=rotarM(matrizAux,tamNuevMatr, modos[posCerra]);
+                liberarM(matrizAux, tamNuevMatr);
+
+
+
+        }
+
+
+
+
+
+
+
+
+        if(posClave==tamaño-1){ //Condicion de terminacion, termino de recorrer la clave
+            ban=false;
+        }
+        posClave++;
+    }
+
+
+
+    if(error){
+        cout<<"No se puede generar una cerradura para esa clave";
+    }else{
+        cout<<"Cerrradura"<<endl;
+        for(int i=0;i<tamaño-1;i++){
+            cout<<cerradura[i]<<" ";
+        }
+
+        cout<<"Modos"<<endl;
+        for(int i=0;i<tamaño-1;i++){
+            cout<<modos[i]<<" ";
+        }
+
+
+    }
+
+    return 0;
+}
